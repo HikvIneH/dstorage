755e5bcaa5333f8ba0e4f8527576c6baec9bb578
Hikvineh <aditya.rifky@flash-coffee.com>
2022-01-02 06:26:58 +0800

    first smart contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const DStorage = artifacts.require("DStorage");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer) {
+  deployer.deploy(DStorage);
 };
